642865d1c6581660d9c6621bea1ab9a5e20c329b
Guilherme Piovezan <guilherme.piovezan00@gmai.com>
2025-08-21 20:28:59 -0300

    Criando os arquivo docker auth-api, product-api e sales-api

---
+++ sales-api/app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+
+const app = express();
+const env = process.env;
+const PORT = env.PORT || 8082;
+
+app.get('/api/status', (req, res) => {
+  return res
+    .status(200)
+    .json({
+      service: "sales-api",
+      version: "1.0.0",
+      status: "up",
+      httpStatus: 200
+    });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
